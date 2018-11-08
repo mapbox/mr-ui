9059937f7672273a06c35a0ac93d4a8bbe38e9f7
Liz Shigetoshi <liz.shigetoshi@mapbox.com>
2018-11-08 13:26:10 -0800

    Add NewTabLink component, examples, and tests

---
+++ src/components/new-tab-link/__tests__/new-tab-link-test-cases.js
@@ -0,0 +1,23 @@
+import NewTabLink from '../new-tab-link';
+
+const testCases = {};
+
+testCases.basic = {
+  description: 'basic',
+  component: NewTabLink,
+  props: {
+    children: 'Hello explorer!'
+  }
+};
+
+testCases.styling = {
+  description: 'styling',
+  component: NewTabLink,
+  props: {
+    children: 'Hello explorer!',
+    className: 'link link--pink',
+    href: 'http://www.mapbox.com'
+  }
+};
+
+export { testCases };
---
+++ src/components/new-tab-link/examples/new-tab-link-a.js
@@ -0,0 +1,11 @@
+/*
+Basic.
+*/
+import React from 'react';
+import NewTabLink from '../new-tab-link';
+
+export default class Example extends React.Component {
+  render() {
+    return <NewTabLink>Hello explorer!</NewTabLink>;
+  }
+}
