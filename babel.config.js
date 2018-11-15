cd82f9a6ab05d381a7529dd0e79c472e49017ede
danswick <dan.swick@gmail.com>
2018-11-15 08:19:23 -0800

    small update

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,4 @@
 'use strict';
 module.exports = {
-  presets: [
-    /* ["@babel/preset-env", { "loose": true }],  */ '@babel/preset-react',
-    '@mapbox/babel-preset-mapbox'
-  ],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        helpers: true,
-        regenerator: false
-        /* "moduleName": "babel-runtime" */
-      }
-    ]
-  ]
+  presets: ['@mapbox/babel-preset-mapbox']
 };
