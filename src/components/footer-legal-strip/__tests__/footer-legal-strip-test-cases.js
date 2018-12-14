ed511e8abd29e1e86a5a14df2e6560450f9fa360
Dan Swick <dan.swick@gmail.com>
2018-12-14 08:01:48 -0800

    FooterLegalStrip patch (#62)

--- src/components/footer-legal-strip/__tests__/footer-legal-strip-test-cases.js
+++ src/components/footer-legal-strip/__tests__/footer-legal-strip-test-cases.js
@@ -1,9 +1,9 @@
-import PageFooter from '../footer-legal-strip';
+import FooterLegalStrip from '../footer-legal-strip';
 
 const testCases = {};
 testCases.basic = {
   description: 'basic',
-  component: PageFooter,
+  component: FooterLegalStrip,
   props: {}
 };
 export { testCases };
--- src/components/footer-legal-strip/examples/footer-legal-strip-basic.js
+++ src/components/footer-legal-strip/examples/footer-legal-strip-basic.js
@@ -2,10 +2,10 @@
 Basic.
 */
 import React from 'react';
-import PageFooter from '../footer-legal-strip';
+import FooterLegalStrip from '../footer-legal-strip';
 
 export default class Example extends React.Component {
   render() {
-    return <PageFooter />;
+    return <FooterLegalStrip />;
   }
 }
