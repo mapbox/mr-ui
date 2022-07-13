16f0ef2c3030f216c4ce45bec58c616d02c8c58e
tristen <tristen.brown@gmail.com>
2022-07-13 14:53:03 -0400

    Update the button component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -6,19 +6,92 @@ type Variant = 'primary' | 'secondary' | 'tertiary' | 'destructive' | 'appPrimar
 type Width = 'small' | 'medium' | 'large' | 'full';
 type Size = 'small' | 'medium' | 'large';
 
+/**
+ * Button properties.
+ */
 interface Props {
+  /**
+   * The button's content. A string is recommended, but you can put an element
+   * in here if you think that's right. If you do, it should be inline-level,
+   * using `<span>`s instead of `<div>`s. ([IconText](#icontext) is
+   * inline-level.)
+   */
   children: ReactNode;
+  /**
+   * A keyword identifying the standard button styling variant to use.
+   *
+   * *The variant is a starting point*: all of the other styling props can be
+   * used to override details.
+   *
+   * The following variants are available:
+   *
+   * - `"primary"`: For primary actions.
+   * - `"secondary"`: For secondary actions.
+   * - `"tertiary"`: For downplayed actions, the ones people shouldn't
+   *   usually want to perform, like cancelling instead of confirming.
+   * - `"destructive"`: For destructive actions, like deleting something.
+   * - `"appPrimary"`: For primary actions in dense apps.
+   * - `"appSecondary"`: For secondary actions in dense apps.
+   */
   variant?: Variant;
+  /**
+   * The Assembly color of the button.
+   */
   color?: string;
+  /**
+   * If `true`, the button will be outlined instead of filled with color.
+   */
   outline?: boolean;
+  /**
+   * If `true`, the button will have (slightly rounded) corners instead of
+   * the full curve they have by default.
+   */
   corners?: boolean;
+  /**
+   * Is it disabled?
+   */
   disabled?: boolean;
+  /**
+   * If `true`, the element will be `block` displayed instead of `inline-block`.
+   *
+   * This is sometimes necessary to get your pixel-perfect layout, if you don't
+   * want the extra line-height that wraps inline elements. Typically, you
+   * should only set `block` to `true` if the parent element is controlling
+   * width (in a layout that uses flexbox, absolute positioning, or floats).
+   */
   block?: boolean;
   onClick?: () => void;
   href?: string;
+  /**
+   * The size of the button: `"small"`, `"medium"`, or `"large"`.
+   *
+   * This accounts for height, outline thickness, text size, and other details.
+   *
+   * This will override whichever defaults are set by your `variant` of choice.
+   */
   size?: Size;
+  /**
+   * The width of the button: `"small"`, `"medium"`, `"large"`, or `"full"`.
+   * For every option but `"full"` this prop determines horizontal padding.
+   * `"full"` sets the button to fill the width of its container.
+   *
+   * This is distinguished from `size` because buttons of the same general size
+   * can vary by width.
+   *
+   * This will override whichever defaults are set by your `variant` of choice.
+   */
   width?: Width;
-  'aria-label'?: string;
+  /**
+   * By default, screenreaders will announce the variant value. If this is not
+   * descriptive enough, use aria-label for a more descriptive label.
+   */
+  ariaLabel?: string;
+  /**
+   * An object of props that you want to pass through to the element that
+   * Button renders. This can be useful if you want to disable
+   * the button, assign an ID for testing, add an ARIA attribute, toss in some
+   * custom style properties, etc.
+   */
   passthroughProps?: {
     [key: string]: string | number | boolean;
   }
@@ -91,7 +164,7 @@ function getColor(variant: Variant): string {
  */
 export default function Button({
   variant = 'primary',
-  'aria-label': ariaLabel,
+  ariaLabel,
   size = getSize(variant),
   width = getWidth(variant),
   color = getColor(variant),
@@ -158,106 +231,3 @@ export default function Button({
     <button type="button" {...buttonProps} />
   );
 }
-
-Button.propTypes = {
-  /**
-   * The button's content. A string is recommended, but you can put an element
-   * in here if you think that's right. If you do, it should be inline-level,
-   * using `<span>`s instead of `<div>`s. ([IconText](#icontext) is
-   * inline-level.)
-   */
-  children: PropTypes.node.isRequired,
-  /**
-   * A keyword identifying the standard button styling variant to use.
-   *
-   * *The variant is a starting point*: all of the other styling props can be
-   * used to override details.
-   *
-   * The following variants are available:
-   *
-   * - `"primary"`: For primary actions.
-   * - `"secondary"`: For secondary actions.
-   * - `"tertiary"`: For downplayed actions, the ones people shouldn't
-   *   usually want to perform, like cancelling instead of confirming.
-   * - `"destructive"`: For destructive actions, like deleting something.
-   * - `"appPrimary"`: For primary actions in dense apps.
-   * - `"appSecondary"`: For secondary actions in dense apps.
-   */
-  variant: PropTypes.oneOf([
-    'primary',
-    'secondary',
-    'tertiary',
-    'destructive',
-    'appPrimary',
-    'appSecondary'
-  ]),
-  /**
-   * A callback that receives the click event.
-   *
-   * You definitely need this if you don't use `href`; but you can also provide
-   * both `onClick` and `href`, and `onClick` will be called when the `<a>` is
-   * clicked.
-   */
-  onClick: PropTypes.func,
-  /**
-   * A URL. If `href` is provided, the button will be an `<a>`.
-   */
-  href: PropTypes.string,
-  /**
-   * The size of the button: `"small"`, `"medium"`, or `"large"`.
-   *
-   * This accounts for height, outline thickness, text size, and other details.
-   *
-   * This will override whichever defaults are set by your `variant` of choice.
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /**
-   * The width of the button: `"small"`, `"medium"`, `"large"`, or `"full"`.
-   * For every option but `"full"` this prop determines horizontal padding.
-   * `"full"` sets the button to fill the width of its container.
-   *
-   * This is distinguished from `size` because buttons of the same general size
-   * can vary by width.
-   *
-   * This will override whichever defaults are set by your `variant` of choice.
-   */
-  width: PropTypes.oneOf(['small', 'medium', 'large', 'full']),
-  /**
-   * If `true`, the button will be outlined instead of filled with color.
-   */
-  outline: PropTypes.bool,
-  /**
-   * The Assembly color of the button.
-   */
-  color: PropTypes.string,
-  /**
-   * If `true`, the button will have (slightly rounded) corners instead of
-   * the full curve they have by default.
-   */
-  corners: PropTypes.bool,
-  /**
-   * If `true`, the element will be `block` displayed instead of `inline-block`.
-   *
-   * This is sometimes necessary to get your pixel-perfect layout, if you don't
-   * want the extra line-height that wraps inline elements. Typically, you
-   * should only set `block` to `true` if the parent element is controlling
-   * width (in a layout that uses flexbox, absolute positioning, or floats).
-   */
-  block: PropTypes.bool,
-  /**
-   * Is it disabled?
-   */
-  disabled: PropTypes.bool,
-  /** 
-   * By default, screenreaders will announce the variant value. If this is not 
-   * descriptive enough, use aria-label for a more descriptive label.
-   */
-  "'aria-label'": PropTypes.string,
-  /**
-   * An object of props that you want to pass through to the element that
-   * Button renders. This can be useful if you want to disable
-   * the button, assign an ID for testing, add an ARIA attribute, toss in some
-   * custom style properties, etc.
-   */
-  passthroughProps: PropTypes.object
-};
