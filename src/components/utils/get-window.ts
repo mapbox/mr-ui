1fad78b8c9ddceff2548a2c714aa1fdfc73b2a46
tristen <tristen.brown@gmail.com>
2022-08-09 11:48:06 -0400

    util fixes for linter

--- src/components/utils/get-window.ts
+++ src/components/utils/get-window.ts
@@ -1,7 +1,7 @@
 // For the purposes of correctly mocking the window object in Jest,
 // this is a safe way of returning it for tests to work as expected.
-export default function getWindow() {
-  if (!window) {
+export default function getWindow(): typeof window {
+  if (typeof window === undefined) {
     throw new Error('window not available');
   }
 
