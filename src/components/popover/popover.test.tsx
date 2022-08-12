32b1895be4d1aa5d94bbeb6fdcc82f54f79995f5
tristen <tristen.brown@gmail.com>
2022-08-12 13:35:23 -0400

    Wrap up tests

--- src/components/popover/popover.test.tsx
+++ src/components/popover/popover.test.tsx
@@ -3,168 +3,100 @@ import Popover from './popover';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-const PopoverTest = () => {
+const PopoverTest = (props) => {
   const [active, setActive] = useState(false);
   return (
-    <Popover active={active} content="A tooltip">
+    <Popover active={active} content="A tooltip" {...props}>
       <button data-testid="trigger" onClick={() => setActive(true)}>trigger</button>
     </Popover>
   )
 }
 
 describe('Popover', () => {
-  test('renders', async () => {
-    render(<PopoverTest />);
-
-    await userEvent.click(screen.getByTestId("trigger"));
-
-    await waitFor(() => {
-      expect(screen.getByRole('dialog')).toBeInTheDocument();
+  describe('basic', () => {
+    test('renders', async () => {
+      const user = userEvent.setup();
+      const { baseElement } = render(<PopoverTest />);
+      await user.click(screen.getByTestId("trigger"));
+      await waitFor(() => {
+        expect(screen.getByRole('dialog')).toBeInTheDocument();
+        expect(baseElement).toMatchSnapshot();
+      });
     });
   });
 
-  /*
-  describe(noDisplayCases.basic.description, () => {
+  describe('dark', () => {
+    const props = {
+      coloring: 'dark'
+    };
 
-  test('renders', async () => {
-    render(<Tooltip content="A tooltip"><span data-testid="trigger">trigger</span></Tooltip>);
-    await userEvent.hover(screen.getByTestId("trigger"));
-
-    await waitFor(() => {
-      expect(screen.getByRole('tooltip')).toBeInTheDocument();
+    test('renders', async () => {
+      const user = userEvent.setup();
+      const { baseElement } = render(<PopoverTest {...props} />);
+      await user.click(screen.getByTestId("trigger"));
+      await waitFor(() => {
+        expect(screen.getByRole('dialog')).toBeInTheDocument();
+        expect(baseElement).toMatchSnapshot();
+      });
     });
   });
 
+  describe('warning', () => {
+    const props = {
+      coloring: 'warning'
+    };
 
-
-    beforeEach(() => {
-      testCase = noDisplayCases.basic;
-      wrapper = shallow(
-        React.createElement(testCase.component, testCase.props)
-      );
-    });
-
-    test('renders', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
-
-  describe(noDisplayCases.dark.description, () => {
-    beforeEach(() => {
-      testCase = noDisplayCases.dark;
-      wrapper = shallow(
-        React.createElement(testCase.component, testCase.props)
-      );
-    });
-
-    test('renders', () => {
-      expect(wrapper).toMatchSnapshot();
+    test('renders', async () => {
+      const user = userEvent.setup();
+      const { baseElement } = render(<PopoverTest {...props} />);
+      await user.click(screen.getByTestId("trigger"));
+      await waitFor(() => {
+        expect(screen.getByRole('dialog')).toBeInTheDocument();
+        expect(baseElement).toMatchSnapshot();
+      });
     });
   });
 
-  describe(noDisplayCases.warning.description, () => {
-    beforeEach(() => {
-      testCase = noDisplayCases.warning;
-      wrapper = shallow(
-        React.createElement(testCase.component, testCase.props)
-      );
-    });
+  describe('no arrow', () => {
+    const props = {
+      hasPointer: false
+    };
 
-    test('renders', () => {
-      expect(wrapper).toMatchSnapshot();
+    test('renders', async () => {
+      const user = userEvent.setup();
+      const { baseElement } = render(<PopoverTest {...props} />);
+      await user.click(screen.getByTestId("trigger"));
+      await waitFor(() => {
+        expect(screen.getByRole('dialog')).toBeInTheDocument();
+        expect(baseElement).toMatchSnapshot();
+      });
     });
   });
 
-  describe(noDisplayCases.noPointer.description, () => {
-    beforeEach(() => {
-      testCase = noDisplayCases.noPointer;
-      wrapper = shallow(
-        React.createElement(testCase.component, testCase.props)
-      );
-    });
+  describe('all options', () => {
+    const props = {
+      placement: 'left',
+      padding: 'none',
+      hasPointer: false,
+      hideWhenAnchorIsOffscreen: true,
+      allowPlacementAxisChange: false,
+      containWithinViewport: false,
+      clickOutsideCloses: false,
+      escapeCloses: false,
+      onExit: () => {},
+      passthroughProps: {
+        'data-testid': 'foo'
+      }
+    };
 
-    test('renders', () => {
-      expect(wrapper).toMatchSnapshot();
+    test('renders', async () => {
+      const user = userEvent.setup();
+      const { baseElement } = render(<PopoverTest {...props} />);
+      await user.click(screen.getByTestId("trigger"));
+      await waitFor(() => {
+        expect(screen.getByRole('dialog')).toBeInTheDocument();
+        expect(baseElement).toMatchSnapshot();
+      });
     });
   });
-
-  describe(noDisplayCases.allProps.description, () => {
-    beforeEach(() => {
-      testCase = noDisplayCases.allProps;
-      wrapper = shallow(
-        React.createElement(testCase.component, testCase.props)
-      );
-    });
-
-    test('renders', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
-  */
 });
-
-/*
-noDisplayCases.basic = {
-  description: 'basic',
-  component: Popover,
-  props: {
-    getAnchorElement: () => {},
-    children: getPopoverContent()
-  }
-};
-
-noDisplayCases.dark = {
-  description: 'dark',
-  component: Popover,
-  props: {
-    getAnchorElement: () => {},
-    children: getPopoverContent(),
-    coloring: 'dark'
-  }
-};
-
-noDisplayCases.warning = {
-  description: 'warning',
-  component: Popover,
-  props: {
-    getAnchorElement: () => {},
-    children: getPopoverContent(),
-    coloring: 'warning'
-  }
-};
-
-noDisplayCases.noPointer = {
-  description: 'no pointer',
-  component: Popover,
-  props: {
-    getAnchorElement: () => {},
-    children: getPopoverContent(),
-    hasPointer: false
-  }
-};
-
-noDisplayCases.allProps = {
-  description: 'all props',
-  component: Popover,
-  props: {
-    getAnchorElement: () => {},
-    children: getPopoverContent(),
-    placement: 'left',
-    padding: 'none',
-    hasPointer: false,
-    hideWhenAnchorIsOffscreen: true,
-    allowPlacementAxisChange: false,
-    containWithinViewport: false,
-    clickOutsideCloses: false,
-    escapeCloses: false,
-    receiveFocus: false,
-    onExit: () => {},
-    ignoreClickWithinElement: () => {},
-    getContainingElement: () => {},
-    passthroughProps: {
-      'data-test': true
-    }
-  }
-};
-*/
-
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -35,3 +35,15 @@ global.DOMRect = class DOMRect {
     return JSON.stringify(this);
   }
 }
+
+global.ResizeObserver = class ResizeObserver {
+  [x: string]: any;
+  constructor(cb: any) {
+    this.cb = cb;
+  }
+  observe() {
+    this.cb([{ borderBoxSize: { inlineSize: 0, blockSize: 0 } }]);
+  }
+  unobserve() {}
+  disconnect() {}
+};
