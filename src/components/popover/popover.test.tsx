80460f14753ef17fc0bf55e66ba822c29b489a2f
tristen <tristen.brown@gmail.com>
2022-08-12 13:55:47 -0400

    Document changes and fix linting errors

--- src/components/popover/popover.test.tsx
+++ src/components/popover/popover.test.tsx
@@ -6,28 +6,37 @@ import userEvent from '@testing-library/user-event';
 const PopoverTest = (props) => {
   const [active, setActive] = useState(false);
   return (
-    <Popover active={active} content="A tooltip" {...props}>
+    <Popover active={active} {...props}>
       <button data-testid="trigger" onClick={() => setActive(true)}>trigger</button>
     </Popover>
   )
 }
 
 describe('Popover', () => {
+  const props = {
+    content: 'Popup content'
+  };
+
   describe('basic', () => {
     test('renders', async () => {
       const user = userEvent.setup();
-      const { baseElement } = render(<PopoverTest />);
+      const { baseElement } = render(<PopoverTest {...props} />);
       await user.click(screen.getByTestId("trigger"));
       await waitFor(() => {
         expect(screen.getByRole('dialog')).toBeInTheDocument();
-        expect(baseElement).toMatchSnapshot();
       });
+      expect(baseElement).toMatchSnapshot();
     });
   });
 
   describe('dark', () => {
+    function renderContent() {
+      return 'Content is rendered'
+    }
+
     const props = {
-      coloring: 'dark'
+      coloring: 'dark',
+      content: renderContent
     };
 
     test('renders', async () => {
@@ -36,14 +45,15 @@ describe('Popover', () => {
       await user.click(screen.getByTestId("trigger"));
       await waitFor(() => {
         expect(screen.getByRole('dialog')).toBeInTheDocument();
-        expect(baseElement).toMatchSnapshot();
       });
+      expect(baseElement).toMatchSnapshot();
     });
   });
 
   describe('warning', () => {
     const props = {
-      coloring: 'warning'
+      coloring: 'warning',
+      content: <span>HI there</span>
     };
 
     test('renders', async () => {
@@ -52,14 +62,15 @@ describe('Popover', () => {
       await user.click(screen.getByTestId("trigger"));
       await waitFor(() => {
         expect(screen.getByRole('dialog')).toBeInTheDocument();
-        expect(baseElement).toMatchSnapshot();
       });
+      expect(baseElement).toMatchSnapshot();
     });
   });
 
   describe('no arrow', () => {
     const props = {
-      hasPointer: false
+      hasPointer: false,
+      content: <div>no arrow</div>
     };
 
     test('renders', async () => {
@@ -68,8 +79,8 @@ describe('Popover', () => {
       await user.click(screen.getByTestId("trigger"));
       await waitFor(() => {
         expect(screen.getByRole('dialog')).toBeInTheDocument();
-        expect(baseElement).toMatchSnapshot();
       });
+      expect(baseElement).toMatchSnapshot();
     });
   });
 
@@ -84,6 +95,12 @@ describe('Popover', () => {
       clickOutsideCloses: false,
       escapeCloses: false,
       onExit: () => {},
+      content: (
+        <>
+          <span className="txt-kbd">Greetings</span>
+          <span className="color-green txt-kbd">human</span>
+        </>
+      ),
       passthroughProps: {
         'data-testid': 'foo'
       }
@@ -95,8 +112,8 @@ describe('Popover', () => {
       await user.click(screen.getByTestId("trigger"));
       await waitFor(() => {
         expect(screen.getByRole('dialog')).toBeInTheDocument();
-        expect(baseElement).toMatchSnapshot();
       });
+      expect(baseElement).toMatchSnapshot();
     });
   });
 });
