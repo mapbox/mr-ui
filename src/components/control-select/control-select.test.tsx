f63ce1f5175b2306c37a0097b42fc5f7203784dc
tristen <tristen.brown@gmail.com>
2022-08-18 14:33:35 -0400

    Rewrite control-select

--- src/components/control-select/control-select.test.tsx
+++ src/components/control-select/control-select.test.tsx
@@ -1,196 +1,155 @@
 import React from 'react';
 import ControlSelect from './control-select';
+import { screen, render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-/*
 describe('ControlSelect', () => {
 
-testCases.basic = {
-  description: 'basic',
-  component: ControlSelect,
-  props: {
-    id: 'testinput',
-    label: 'Basic',
-    options: [
-      { label: 'Option 1', value: 'Option value 1' },
-      { label: 'Option 2', value: 'Option value 2' }
-    ],
-    onChange: jest.fn()
-  }
-};
-
-  describe(testCases.basic.description, () => {
-    beforeEach(() => {
-      testCase = testCases.basic;
-      wrapper = shallow(
-        React.createElement(testCase.component, testCase.props)
-      );
-    });
+  describe('basic', () => {
+    const mockOnChange = jest.fn();
+    const props = {
+      id: 'testinput',
+      label: 'Basic',
+      options: [
+        { label: 'Option 1', value: 'Option value 1' },
+        { label: 'Option 2', value: 'Option value 2' }
+      ],
+      onChange: mockOnChange
+    };
 
     test('renders', () => {
-      expect(wrapper).toMatchSnapshot();
+      const { baseElement } = render(<ControlSelect {...props} />)
+      expect(baseElement).toMatchSnapshot();
     });
 
-    test('onChange gets called with input id and value', () => {
-      const mockEvent = {
-        target: {
-          value: 'foo'
+    test('onChange is called with input id and value', async () => {
+      render(<ControlSelect {...props} />)
+
+      await userEvent.selectOptions(screen.getByTestId('testinput-select'), 'Option value 1');
+
+      await waitFor(() => {
+        expect(mockOnChange).toHaveBeenCalledTimes(1);
+      });
+
+      await waitFor(() => {
+        expect(mockOnChange).toHaveBeenCalledWith('Option value 1', 'testinput');
+      });
+    });
+  });
+
+  describe('basic', () => {
+    const mockOnChange = jest.fn();
+    const props = {
+      id: 'testinput',
+      label: 'Basic',
+      disabled: true,
+      options: [
+        { label: 'Option 1', value: 'Option value 1' },
+        { label: 'Option 2', value: 'Option value 2' }
+      ],
+      onChange: mockOnChange
+    };
+
+    test('renders', () => {
+      const { baseElement } = render(<ControlSelect {...props} />)
+      expect(baseElement).toMatchSnapshot();
+    });
+
+    test('onChange is *not* called with input id and value', async () => {
+      render(<ControlSelect {...props} />)
+
+      await userEvent.selectOptions(screen.getByTestId('testinput-select'), 'Option value 1');
+
+      await waitFor(() => {
+        expect(mockOnChange).toHaveBeenCalledTimes(0);
+      });
+    });
+  });
+
+  describe('grouped dropdown options', () => {
+    const props = {
+      id: 'testinput',
+      label: 'Basic',
+      disabled: true,
+      options: [
+        {
+          label: 'Group One',
+          options: [
+            {
+              label: 'Foo',
+              value: 'foo'
+            },
+            {
+              label: 'Bar',
+              value: 'bar'
+            }
+          ]
+        },
+        {
+          label: 'Group two',
+          options: [
+            {
+              label: 'Baz',
+              value: 'baz'
+            },
+            {
+              label: 'Qux',
+              value: 'qux'
+            }
+          ]
         }
-      };
-      wrapper.find('select').props().onChange(mockEvent);
-      wrapper.update();
-      expect(wrapper).toMatchSnapshot();
-      expect(testCase.props.onChange).toHaveBeenCalledTimes(1);
-      expect(testCase.props.onChange).toHaveBeenCalledWith('foo', 'testinput');
+      ],
+      onChange: jest.fn()
+    };
+
+    test('renders', () => {
+      const { baseElement } = render(<ControlSelect {...props} />)
+      expect(baseElement).toMatchSnapshot();
     });
   });
 
-testCases.disabled = {
-  description: 'disabled',
-  component: ControlSelect,
-  props: {
-    id: 'disabled',
-    disabled: true,
-    label: 'Disabled',
-    options: [
-      { label: 'Option 1', value: 'Option value 1' },
-      { label: 'Option 2', value: 'Option value 2' }
-    ],
-    onChange: jest.fn()
-  }
-};
-
-  describe(testCases.disabled.description, () => {
-    beforeEach(() => {
-      testCase = testCases.disabled;
-      wrapper = shallow(
-        React.createElement(testCase.component, testCase.props)
-      );
-    });
+  describe('supported values', () => {
+    const props = {
+      id: 'value',
+      label: 'Basic',
+      options: [
+        { label: 'Empty string', value: '' },
+        { label: 'String', value: 'foo' },
+        { label: 'Number', value: 123 }
+      ],
+      onChange: jest.fn()
+    };
 
     test('renders', () => {
-      expect(wrapper).toMatchSnapshot();
+      const { baseElement } = render(<ControlSelect {...props} />)
+      expect(baseElement).toMatchSnapshot();
     });
   });
 
-testCases.grouped = {
-  description: 'grouped dropdown options',
-  component: ControlSelect,
-  props: {
-    id: 'grouped',
-    label: 'Grouped dropdown options',
-    onChange: jest.fn(),
-    options: [
-      {
-        label: 'Group One',
-        options: [
-          {
-            label: 'Foo',
-            value: 'foo'
-          },
-          {
-            label: 'Bar',
-            value: 'bar'
-          }
-        ]
-      },
-      {
-        label: 'Group two',
-        options: [
-          {
-            label: 'Baz',
-            value: 'baz'
-          },
-          {
-            label: 'Qux',
-            value: 'qux'
-          }
-        ]
-      }
-    ]
-  }
-};
-
-  describe(testCases.grouped.description, () => {
-    beforeEach(() => {
-      testCase = testCases.grouped;
-      wrapper = shallow(
-        React.createElement(testCase.component, testCase.props)
-      );
-    });
+  describe('all options', () => {
+    const props = {
+      id: 'allOptions',
+      options: [
+        { label: 'Option 1', value: 'Option value 1' },
+        { label: 'Option 2', value: 'Option value 2' },
+        { label: 'Option 3', value: 'Option value 3', disabled: true }
+      ],
+      validationError: 'oh no!',
+      value: 'Option value 2',
+      autoFocus: true,
+      optional: true,
+      label: 'All options',
+      aside: <span>Aside text</span>,
+      themeControlSelectContainer: 'color-white',
+      themeControlSelect: 'select--red',
+      themeControlWrapper: 'bg-red-light',
+      themeLabel: 'color-white',
+      onChange: jest.fn()
+    };
 
     test('renders', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
-
-testCases.values = {
-  description: 'Supported values',
-  component: ControlSelect,
-  props: {
-    id: 'value',
-    label: 'Basic',
-    options: [
-      { label: 'Empty string', value: '' },
-      { label: 'String', value: 'foo' },
-      { label: 'Number', value: 123 }
-    ],
-    onChange: jest.fn()
-  }
-};
-
-  describe(testCases.values.description, () => {
-    beforeEach(() => {
-      testCase = testCases.values;
-      wrapper = shallow(
-        React.createElement(testCase.component, testCase.props)
-      );
-    });
-
-    test('renders', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
-
-testCases.allOptions = {
-  description: 'all options',
-  component: ControlSelect,
-  props: {
-    id: 'allOptions',
-    options: [
-      { label: 'Option 1', value: 'Option value 1' },
-      { label: 'Option 2', value: 'Option value 2' },
-      { label: 'Option 3', value: 'Option value 3', disabled: true }
-    ],
-    validationError: 'oh no!',
-    value: 'Option value 2',
-    autoFocus: true,
-    optional: true,
-    label: 'All options',
-    aside: <span>Aside text</span>,
-    themeControlSelectContainer: 'color-white',
-    themeControlSelect: 'select--red',
-    themeControlWrapper: 'bg-red-light',
-    themeLabel: 'color-white',
-    onChange: jest.fn()
-  }
-};
-
-  describe(testCases.allOptions.description, () => {
-    beforeEach(() => {
-      testCase = testCases.allOptions;
-      wrapper = shallow(
-        React.createElement(testCase.component, testCase.props)
-      );
-    });
-
-    test('extra props works', () => {
-      const props = wrapper.find('option').last().props();
-      expect(props).toHaveProperty('disabled', true);
-    });
-
-    test('renders', () => {
-      expect(wrapper).toMatchSnapshot();
+      const { baseElement } = render(<ControlSelect {...props} />)
+      expect(baseElement).toMatchSnapshot();
     });
   });
 });
-*/
---
+++ src/components/control-select/examples/control-select-example-basic-b.tsx
@@ -0,0 +1,53 @@
+/*
+Basic.
+*/
+import React from 'react';
+import ControlSelect from '../control-select';
+
+const groupedOptions = [
+  {
+    label: 'Group One',
+    options: [
+      {
+        label: 'Foo',
+        value: 'foo'
+      },
+      {
+        label: 'Bar',
+        value: 'bar'
+      }
+    ]
+  },
+  {
+    label: 'Group two',
+    options: [
+      {
+        label: 'Baz',
+        value: 'baz'
+      },
+      {
+        label: 'Qux',
+        value: 'qux',
+        disabled: true
+      }
+    ]
+  }
+];
+
+export default function Example() {
+  return (
+    <ControlSelect
+      id="group"
+      label="Choose from a group"
+      validationError="oh no!"
+      value="baz"
+      optional={true}
+      aside={<span>Aside text</span>}
+      themeControlSelect="select--white bg-blue px12"
+      onChange={(value, id) => {
+        console.log(value, id);
+      }}
+      options={groupedOptions}
+    />
+  );
+}
