b5f639f321ca7b59b3973ca9915abe1a0430d1a2
Oana Badiu <oana.badiu@mapbox.com>
2024-07-17 13:05:39 +0300

    Fix vulnerabilities

--- scripts/build-package.js
+++ scripts/build-package.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 const path = require('path');
 const del = require('del');
 const execa = require('execa');
-const cpy = require('cpy');
 const makeDir = require('make-dir');
 
 const rootDir = path.resolve(__dirname, '..');
@@ -32,11 +31,16 @@ function compileComponents() {
   );
 }
 
-// Copy non-src files that we want in the package.
 function copyFiles() {
-  return cpy(['LICENSE', 'CHANGELOG.md', 'README.md'], outputDir, {
-    cwd: rootDir
+  const filesToCopy = ['LICENSE', 'CHANGELOG.md', 'README.md'];
+
+  const copyPromises = filesToCopy.map((file) => {
+    const srcPath = path.join(rootDir, file);
+    const destPath = path.join(outputDir, path.basename(file));
+    return fs.promises.copyFile(srcPath, destPath);
   });
+
+  return copyPromises;
 }
 
 // Create a package.json that is ready to be published.
